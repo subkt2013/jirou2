605c634eacbbc1807c15fffdcfd8ca2f1746e96e
subkt2013 <subkt2013@gmail.com>
2020-07-04 19:03:07 +0900

    冗長なマイグレーションファイルの一本化

--- database/migrations/2019_01_20_161408_create_blogposts_table.php
+++ database/migrations/2019_01_20_161408_create_blogposts_table.php
@@ -13,11 +13,14 @@ class CreateBlogpostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('blogposts', function (Blueprint $table) {
+        Schema::create('blog_posts', function (Blueprint $table) {
             $table->bigInteger('id',true)->unsigned();
             $table->string('store_name');
             $table->bigInteger('wait_people')->unsigned();
             $table->text('introduction', 65535)->nullable()->comment('[店舗紹介]');
+            $table->string('title');
+            $table->text('content');
+            $table->string('cover_image');
             $table->timestamps();
         });
     }
@@ -29,6 +32,6 @@ class CreateBlogpostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('blogposts');
+        Schema::dropIfExists('blog_posts');
     }
 }
