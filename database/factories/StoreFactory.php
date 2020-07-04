5d7610619a7743ff396dcee8fad46c88578203f3
subkt2013 <subkt2013@gmail.com>
2020-07-04 19:03:39 +0900

    使ってない変数の削除

--- database/factories/StoreFactory.php
+++ database/factories/StoreFactory.php
@@ -8,10 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Store::class, function (Faker $faker) {
     return [
         'store_name' => $faker->name,
-        //'image_path' => $faker->url,
-        //'alt' => $faker->sentence(20),
         'wait_people' =>  $faker->numberBetween(1, 20), 
-       // 'estemated_wait_time' => $faker->numberBetween(1, 20), 
         'introduction' => $faker->paragraph(),
       
     ];
