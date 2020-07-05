e1c4c3ce69c25c829f063c5c900550cb9c1a5130
subkt2013 <subkt2013@gmail.com>
2020-07-05 23:43:37 +0900

    topのメニューすっきりした

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,24 +27,7 @@
     <header>
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light bg-light"> 
-        <a class="navbar-brand" href="/">TOP</a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    エリア
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink"> <a class="dropdown-item" href="#">NBA</a> </div>
-                    </li>
-                    <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    最新更新順
-                    </a>
-                    </li>
-                    <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    人数
-                    </a>
-                    </li>
-                </ul>
-            </div>
+            <a class="navbar-brand" href="/">TOP</a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         </nav>
         <div class="row">
             <div class="col-sm-12">
@@ -56,7 +39,7 @@
                 </div>
             </div>
         </div>
-        </div>
+    </div>
     </header>
 
         <main class="py-4">
