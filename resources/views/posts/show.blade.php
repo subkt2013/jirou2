05bdcce6cb04b174f46f124e4e421f9e09e48203
tate12 <tate@sbicb.co.jp>
2020-07-06 12:27:40 +0900

    店情報の表示変更

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,9 +5,9 @@
     <div class="card" style="width: 18rem;">
         <img src="/storage/img/{{ $post->cover_image}}" class="card-img-top" alt="画像">
         <div class="card-body">
-            <h5 class="card-title">店名: {{$post->store_name}}</h5>
+            <h5 class="card-title">{{$post->store_name}}</h5>
             <p class="card-text">
-            説明: Some quick example text to build on the card title and make up the bulk of the card's content. <br>
+            {!! nl2br(e($post->introduction)) !!}<br>
             行列: {{$post->wait_people}} 人待ち <br>
             更新日時: {{$post->updated_at}} <br>
             </p>
